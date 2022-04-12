da5124d2e74911594679a10b97178f5337d6a50f
DavideSegullo <davidesegullo@gmail.com>
2022-04-12 19:33:37 +0200

    feat: :sparkles: add feature #1

--- code/src/utils/address.ts
+++ code/src/utils/address.ts
@@ -1,13 +1,15 @@
 export const searchValidAddress = (text: string, prefix = 'bitsong') => {
-  const regex = new RegExp(`${prefix}1[a-zA-Z0-9]{38}`, 'm');
+  const regex = new RegExp(`${prefix}1[a-zA-Z0-9]{38}`, 'gm');
   let address: string;
 
   let m: RegExpExecArray;
 
   if ((m = regex.exec(text)) !== null) {
-    m.forEach((match) => {
-      address = match;
-    });
+    if (m.length === 1) {
+      m.forEach((match) => {
+        address = match;
+      });
+    }
   }
 
   return address;
